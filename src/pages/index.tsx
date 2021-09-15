4deb198e7cf1a964322acffec5471cd491726a92
benfurber <ben@fu.rber.io>
2021-09-15 10:20:01 +0100

    ✨ Add CategoryList to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
 
 import Page from '../components/Page'
-import { Container, Hero } from '../components'
+import { CategoryList, Container, Hero } from '../components'
 import IndexLayout from '../layouts'
 
 const IndexPage = () => (
@@ -13,10 +12,7 @@ const IndexPage = () => (
           imageUrl="./images/hero/home.jpg"
           title="Easily find the perfect plant subscription for you"
         />
-        <h1>Hi people</h1>
-        <p>Welcome to your new Gatsby site.</p>
-        <p>Now go build something great.</p>
-        <Link to="/page-2/">Go to page 2</Link>
+        <CategoryList />
       </Container>
     </Page>
   </IndexLayout>

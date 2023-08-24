cd94ab0dac178cd5fccef187655df41ae167f2bb
Ben Furber <ben.furber@googlemail.com>
2023-08-24 19:59:25 +0100

    🗑 Drop tags data

---
+++ src/data/tags.example.ts
@@ -0,0 +1,18 @@
+import type { ITag } from "../types";
+
+export const tags: ITag[] = [
+  {
+    id: "355",
+    label: "Epipremnum",
+    slug: "epipremnum",
+    plantIds: ["0", "8", "21", "59", "181", "234", "244", "332"],
+    plantCount: 8,
+  },
+  {
+    id: "356",
+    label: "Marble",
+    slug: "marble",
+    plantIds: ["0"],
+    plantCount: 1,
+  },
+];
